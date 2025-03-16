7f65c638b3daf9b9a3601f81c087c5a36430f7e5
Maxson Almeida <maxsonferovante@gmail.com>
2025-03-16 17:24:48 -0300

    refactor custom expection ConflictException

--- src/main/java/com/things/users/infrastructure/expection/ConflictException.java
+++ src/main/java/com/things/users/infrastructure/expection/ConflictException.java
@@ -1,6 +1,19 @@
 package com.things.users.infrastructure.expection;
 
-public class ConflictException extends Throwable {
-    public ConflictException(String emailExist) {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+// Define uma exceção personalizada para conflitos
+@ResponseStatus(HttpStatus.CONFLICT)
+public class ConflictException extends RuntimeException {
+
+    // Construtor que aceita uma mensagem personalizada
+    public ConflictException(String message) {
+        super(message);
+    }
+
+    // Construtor que aceita uma mensagem personalizada e uma causa
+    public ConflictException(String message, Throwable cause) {
+        super(message, cause);
     }
 }
